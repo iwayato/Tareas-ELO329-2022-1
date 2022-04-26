26f822cd830d285fc2392166f73b6872ff2d6264
iwayato <iwayatomoaki@gmail.com>
2022-04-26 14:08:24 -0400

    Update

--- Stage2HelpCode/LampControl.java
+++
@@ -1,8 +0,0 @@
-public class LampControl extends DomoticDeviceControl {
-    public LampControl(int channel, Cloud c){
-        // ???
-    }
-    public void pressPower(){
-        cloud.changeLampPowerState(channel);
-    }
-}
